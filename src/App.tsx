2bce179b35385dd3a30816b43a9c9b605fc0d6ce
zhouhuafei <1123486116@qq.com>
2022-08-17 18:49:26 +0800

    feat: 🎸 css module

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import helloWorld from './helloWorld.module.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className={helloWorld.redText}>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
