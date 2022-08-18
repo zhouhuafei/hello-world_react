e56cfb0fa2d20b5706258332e2764c1255aa4132
zhouhuafei <1123486116@qq.com>
2022-08-18 15:43:04 +0800

    feat: 🎸 alias

--- config-overrides.js
+++ config-overrides.js
@@ -2,15 +2,20 @@
 // https://github.com/arackaf/customize-cra
 
 const {
+  addWebpackAlias,
   addPostcssPlugins,
   adjustStyleLoaders,
   override
 } = require('customize-cra')
+const path = require('path')
+const resolve = dir => path.resolve(__dirname, dir)
 
 module.exports = {
   webpack: function (config, env) {
 
     override(
+      // alias
+      addWebpackAlias({ '@': resolve('./src') }),
       // px2vw
       addPostcssPlugins([require('postcss-px-to-viewport')({ viewportWidth: 375 })]), // ...TODO 无效
       // 共享scss变量
--- src/components/HelloWorld/index.tsx
+++ src/components/HelloWorld/index.tsx
@@ -1,6 +1,11 @@
 import scss from './index.module.scss'
+import { px2vw } from '@/utils'
 
-export default function () {
+export default function HelloWorld () {
+
+  const vw = px2vw(375)
+  console.log('%c%s', 'color: green;', vw)
+
   return (
     <div className={scss.brandColorText}>hello world</div>
   )
