2ad8355b56d3338024113935f47014e76802fb1a
zhouhuafei <1123486116@qq.com>
2022-08-18 15:50:52 +0800

    style: 💄 components HelloWorld

--- src/components/HelloWorld/index.tsx
+++ src/components/HelloWorld/index.tsx
@@ -1,10 +1,12 @@
 import scss from './index.module.scss'
 import { px2vw } from '@/utils'
 
+const vw = px2vw(375)
+console.log('%c%s等于%s', 'color: green;', '在本项目中375px', vw)
+
 export default function HelloWorld () {
 
-  const vw = px2vw(375)
-  console.log('%c%s', 'color: green;', vw)
+  console.log('HelloWorld')
 
   return (
     <div className={scss.brandColorText}>hello world</div>
