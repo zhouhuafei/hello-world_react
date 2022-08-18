9f38e9ac8ce7d3b794851f5ba56072964471fb42
zhouhuafei <1123486116@qq.com>
2022-08-18 16:22:33 +0800

    docs: ✏️ 在react中没有全局组件的概念

--- src/components/HelloWorld/index.tsx
+++ src/components/HelloWorld/index.tsx
@@ -2,6 +2,7 @@ import scss from './index.module.scss'
 import { px2vw } from '@/utils'
 
 const vw = px2vw(375)
+console.log('%c%s', 'color: green;', '在react中没有全局组件的概念')
 console.log('%c%s等于%s', 'color: green;', '在本项目中375px', vw)
 
 export default function HelloWorld () {
