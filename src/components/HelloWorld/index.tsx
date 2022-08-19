d88e5bd8faddb0342539904771687d52e48accdf
zhouhuafei <1123486116@qq.com>
2022-08-19 17:42:59 +0800

    style: 💄 HelloWorld

--- src/components/HelloWorld/index.tsx
+++ src/components/HelloWorld/index.tsx
@@ -10,25 +10,35 @@ console.log('%c%s等于%s', 'color: green;', '在本项目中375px', vw)
 export default function HelloWorld () {
   console.log('HelloWorld')
 
-  let [countFields, setCountFields] = useState({ fields: { count: 0 } })
+  let [count, setCount] = useState(11)
+  let [calculatorFields, setCalculatorFields] = useState({ calculator: { count: 21, name: '计算器' } })
 
   useEffect(() => {
     console.log('useEffect')
-  }, [countFields])
+    console.log('count', count)
+    console.log('calculatorFields.calculator.count', count)
+  }, [
+    count,
+    calculatorFields
+  ])
 
   function handleCounter (e: MouseEvent) {
     console.log('MouseEvent', e)
-    countFields.fields.count++
-    setCountFields({ ...countFields })
+    count++
+    setCount(count)
+
+    calculatorFields.calculator.count++
+    setCalculatorFields(calculatorFields)
   }
 
   return (
     <div className={scss.hello} onClick={handleCounter}>
       <SvgIcon iconName="home" iconClass={scss.icon} />
       <div className={scss.text}>
+        <span className={scss.textItem}>{count}</span>
         <span className={scss.textItem}>hello</span>
         <span className={scss.textItem}>world</span>
-        <span className={scss.textItem}>{countFields.fields.count}</span>
+        <span className={scss.textItem}>{calculatorFields.calculator.count}</span>
       </div>
       <SvgIcon iconName="mine" iconClass={scss.icon} />
     </div>
