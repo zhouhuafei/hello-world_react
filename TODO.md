e69839687bc38a865a4875171280ffabfc5263ad
zhouhuafei <1123486116@qq.com>
2022-08-22 19:17:10 +0800

    feat: 🎸 换成@craco/craco打包

--- TODO.md
+++ TODO.md
@@ -1,3 +1,1 @@
-...TODO px2vw
 ...TODO test 组件测试识别不到@符号
-...TODO 换成@craco/craco打包
---
+++ craco.config.js
@@ -0,0 +1,45 @@
+// https://github.com/dilanx/craco
+const sassResourcesLoader = require('craco-sass-resources-loader')
+const svgSpriteLoader = require('craco-plugin-svg-sprite')
+const path = require('path')
+const resolve = dir => path.resolve(__dirname, dir)
+
+module.exports = {
+  webpack: {
+    // alias
+    alias: { '@': resolve('./src') }
+  },
+  style: {
+    postcss: {
+      mode: 'extends',
+      loaderOptions: {
+        postcssOptions: {
+          ident: 'postcss',
+          plugins: [
+            // px2vw
+            ['postcss-px-to-viewport-8-plugin', { viewportWidth: 375 }]
+          ]
+        }
+      }
+    }
+  },
+  plugins: [
+    // svg-sprite-loader会把被引入的svg文件，塞到一个个symbol标签中，合成一个大的svg标签，并插入到body标签中。
+    // 封装SvgIcon组件时，在svg标签的子元素use标签上，给xlinkHref属性附加symbol标签的id，即可正常显示出对应的svg图标。
+    {
+      plugin: svgSpriteLoader,
+      options: {
+        include: resolve('./src/icons'),
+        compress: true,
+        svgoConfig: {},
+        spriteLoaderConfig: {},
+        svgPrefixName: 'icon'
+      }
+    },
+    // 共享scss变量
+    {
+      plugin: sassResourcesLoader,
+      options: { resources: ['index'].map(v => `./src/scss/config/${v}.scss`) }
+    }
+  ]
+}
