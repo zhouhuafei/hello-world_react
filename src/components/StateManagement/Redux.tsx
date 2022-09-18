f03561875cb3ffa84f3ba93e57275e12d60f1c0f
zhouhuafei <1123486116@qq.com>
2022-09-18 19:45:10 +0800

    feat: 🎸 useEffect的正确使用方式

--- src/components/StateManagement/Redux.tsx
+++ src/components/StateManagement/Redux.tsx
@@ -2,18 +2,17 @@ import { store, incremented, decremented } from 'src/stores/redux'
 import { useEffect, useState } from 'react'
 
 function Redux () {
+  const [now, setNow] = useState(0)
 
   useEffect(() => {
+    // 订阅需要放到useEffect中，否则会有bug。因为函数本身会多次触发。而useEffect并不会频繁触发。
+    const unsubscribe = store.subscribe(() => {
+      setNow(Date.now()) // 通过setState重新渲染组件 - 如果嫌弃不优化 - 可以使用react-redux
+    })
     return () => {
       unsubscribe() // 别忘了取消监听 - <React.StrictMode>导致开发环境异常 - 所以把它注释掉了
     }
-  })
-
-  const [now, setNow] = useState(0)
-
-  const unsubscribe = store.subscribe(() => {
-    setNow(Date.now()) // 通过setState重新渲染组件 - 如果嫌弃不优化 - 可以使用react-redux
-  })
+  }, [])
 
   function onClickIncrement () {
     store.dispatch(incremented(1))
